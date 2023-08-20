e6a2067f9b818f05ef5511c65d19027ee5eb169c
Dakota Schramm <william.schramm11@gmail.com>
2023-08-19 20:28:47 -0500

    Fix type on links within header.

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -2,11 +2,11 @@ import React, { ReactNode } from 'react'
 import Link from 'next/link';
 
 const Header = (
-  { links, children }: { links: ReactNode, children: ReactNode }
+  { links, children }: { links?: ReactNode, children: ReactNode }
 ) => {
   return (
     <header className='fixed top-0 flex items-center justify-between w-full h-20 px-4 text-black bg-white'>
-      <div className='flex space-x-2'>
+      <div className='flex items-center justify-end space-x-2'>
         <h1 className='text-xl font-bold font-pop text-main-offline'>
           <Link href="/feed">Flipnote -- Legacy Viewer</Link>
         </h1>
