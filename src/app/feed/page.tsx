d0669114fe4270a5be0a06db4509dc64f9282976
Dakota Schramm <william.schramm11@gmail.com>
2023-08-20 23:22:57 -0500

    Add new feed function using favorites.

--- src/app/feed/page.tsx
+++ src/app/feed/page.tsx
@@ -1,22 +1,40 @@
-import { BulletinBoard } from '@/components/bulletin-board';
+import { User } from '@prisma/client';
+
+import { BulletinBoard } from './bulletin-board';
+import { storageAvailable } from '../_lib/storageAvailable';
 
 // TODO: Get fonts from sudomemo site
 // TODO: Get colors from sudomemo site
 // TODO: Figure out if can invaldate using a hook?
-async function fetchUsers() {
-  const res = await fetch(`${process.env.URL}/api/users`, {
-    next: { revalidate: 60 } // figure out if using this one or one defined in route
-  })
-  const data = await res.json()
 
-  return data.users
+export const revalidate = 60
+
+async function fetchUsers() {
+  let feed: User[] | undefined
+  (() => {
+    if (!storageAvailable("localStorage")) return
+  
+    const favoritesJsonString = localStorage.getItem('favorites')
+    if (!favoritesJsonString) return
+
+    const res = JSON.parse(favoritesJsonString)
+    const data = res.data
+    console.log(": ", localStorage, data)
+
+    if (data.users.length === 0) return
+
+    // Get users using ids from favorites
+    // feed = ...
+  })();
+
+  if (!feed) feed = await fetchDefaultFeed()
+  return feed
 }
 
 // TODO: Add error handling for when user has
 // no additional flipnotes to display
 export default async function Feed() {
   const users = await fetchUsers()
-
   if (!users) throw new Error('Users failed to load')
 
   return (
@@ -24,5 +42,12 @@ export default async function Feed() {
       <BulletinBoard users={users} />
     </main>
   )
+}
+
+async function fetchDefaultFeed() {
+  const res = await fetch(`${process.env.URL}/api/users`)
+  const data = await res.json()
+
+  return data.users
 }
 
