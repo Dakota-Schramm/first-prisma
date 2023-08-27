3fe9935e1ff80f85ec4dfb6c1423416d8d568a17
Dakota Schramm <william.schramm11@gmail.com>
2023-08-26 23:29:37 -0500

    Move timeout effect to bulletin.

--- src/app/_components/flipnote/flipnote.tsx
+++ src/app/_components/flipnote/flipnote.tsx
@@ -10,27 +10,10 @@ import LoadingFrog from '../loading-frog';
 import frog from './frog.png';
 
 import { IFRAME_BASE_URL as BASE_URL } from '@/app/_utils/constants';
-import { AnalyticsContext } from '@/app/_contexts/analytics';
 
 // TODO: Fix error here where flipnotes don't lazy load
 const FlipnoteContent = ({ flipnoteId, userId }) => {
-  const { analytics, setAnalytics } = useContext(AnalyticsContext);
   const [isLoaded, setIsLoaded] = useState(false);
-  const [hasBeenViewed, setHasBeenViewed] = useState(false);
-
-  // TODO: Move to Bulletin-Board
-  useEffect(() => {
-    const monitor = setInterval(() => {
-      if (hasBeenViewed) return;
-      const elem = document.activeElement;
-      const elemIsIframe = elem?.tagName === 'IFRAME';
-      if (!elemIsIframe) return;
-      const [aUId, aFId] = elem.id.split('-');
-      console.log({ aUId, aFId });
-      setHasBeenViewed(true);
-      handleAnalyticsUpdate(aUId, aFId, setAnalytics);
-    }, 1000 * 15);
-  }, []);
 
   return (
     <div className='relative'>
@@ -53,34 +36,6 @@ const FlipnoteContent = ({ flipnoteId, userId }) => {
     </div>
   );
 };
-
-function handleAnalyticsUpdate(userId: string, flipnoteId: string, update) {
-  update((prevUsers) => {
-    console.log(prevUsers);
-    log(`Analytics updated for user ${userId}`);
-    let increment = 1;
-
-    const isNewUser = !(userId in (prevUsers ?? {}));
-    const isNewFlipnote =
-      isNewUser || !(flipnoteId in (prevUsers[userId] ?? {}));
-    let flipnoteToUpdate = !isNewFlipnote
-      ? { [flipnoteId]: prevUsers[userId][flipnoteId] + 1 }
-      : { [flipnoteId]: 1 };
-
-    let userToUpdate = !isNewUser
-      ? { [userId]: { ...flipnoteToUpdate, ...prevUsers[userId] } }
-      : { [userId]: { ...flipnoteToUpdate } };
-
-    if (!isNewFlipnote) increment += prevUsers[userId][flipnoteId];
-
-    const updatedUserAnalytics = {
-      ...prevUsers,
-      ...userToUpdate,
-    };
-
-    return updatedUserAnalytics;
-  });
-}
 
 type FlipnoteProps = {
   id: string;
@@ -114,6 +69,7 @@ const Flipnote = ({
     if (!isVisible || !isLast) return;
     handleGetNextFlipnotes();
   }, [isVisible]);
+
 
   return (
     <details
--- src/app/bulletin-board.tsx
+++ src/app/bulletin-board.tsx
@@ -1,45 +1,93 @@
 'use client';
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import { User } from '@prisma/client';
 
 import { Flipnote } from '@/app/_components/flipnote';
 import { useFlipnotes } from '@/hooks/useFlipnotes';
 import useFeed from './_hooks/useFeed';
 import FeedSelector from './feed-selector';
+import log from '@/app/_utils/log';
+import { AnalyticsContext } from '@/app/_contexts/analytics';
 
 export const BulletinBoard = () => {
-  const [feedType, setFeedType] = useState('all');
-  const [feed, isLoaded] = useFeed();
-  const { flipnotes, handleGetNextFlipnotes } = useFlipnotes(feed);
+  const [feedData, setFeedData] = useFeed();
+  const { flipnotes, handleGetNextFlipnotes } = useFlipnotes(feedData.users);
+  const [hasBeenViewed, setHasBeenViewed] = useState(
+    flipnotes.map((fId) => ({ [fId]: false }))
+  );
+  const { analytics, setAnalytics } = useContext(AnalyticsContext);
 
   useEffect(() => {
     handleGetNextFlipnotes();
-  }, [isLoaded]);
+  }, []);
+
+  // TODO: Move to Bulletin-Board
+  useEffect(() => {
+    const monitor = setInterval(() => {
+      if (hasBeenViewed) return;
+      const elem = document.activeElement;
+      const elemIsIframe = elem?.tagName === 'IFRAME';
+      if (!elemIsIframe) return;
+      const [aUId, aFId] = elem.id.split('-');
+      console.log({ aUId, aFId });
+      setHasBeenViewed((prev) => {
+        return { ...{ [aFId]: true }, ...prev };
+      });
+      handleAnalyticsUpdate(aUId, aFId, setAnalytics);
+    }, 1000 * 15);
+  }, []);
 
   return (
     <>
-      <FeedSelector {...{ feedType, setFeedType }} />
+      <FeedSelector
+        feedType={feedData.type}
+        setFeedType={(t) => setFeedData((prev) => ({ ...prev, type: t }))}
+      />
       <section className='flex flex-col items-center justify-between min-h-screen p-24'>
-        {flipnotes.map(({ id, userId }, idx) => {
-          const userName = getUserName(feed, userId);
-
-          return (
-            <Flipnote
-              key={id}
-              isLast={idx === flipnotes.length - 1}
-              {...{ id, userId, userName, handleGetNextFlipnotes }}
-            />
-          );
-        })}
+        {flipnotes.map(({ id, userId }, idx) => (
+          <Flipnote
+            key={id}
+            isLast={idx === flipnotes.length - 1}
+            userName={getUserName(feed, userId)}
+            {...{ id, userId, handleGetNextFlipnotes }}
+          />
+        ))}
       </section>
     </>
   );
 };
 
-function getUserName(users: User[], userId: string) {
-  const user = users.find((user) => user.id === userId);
-  if (user === undefined) throw new Error('User not found');
+function handleAnalyticsUpdate(userId: string, flipnoteId: string, update) {
+  update((prevUsers) => {
+    console.log(prevUsers);
+    log(`Analytics updated for user ${userId}`);
+    let increment = 1;
 
-  return user.name;
+    const isNewUser = !(userId in (prevUsers ?? {}));
+    const isNewFlipnote =
+      isNewUser || !(flipnoteId in (prevUsers[userId] ?? {}));
+    let flipnoteToUpdate = !isNewFlipnote
+      ? { [flipnoteId]: prevUsers[userId][flipnoteId] + 1 }
+      : { [flipnoteId]: 1 };
+
+    let userToUpdate = !isNewUser
+      ? { [userId]: { ...flipnoteToUpdate, ...prevUsers[userId] } }
+      : { [userId]: { ...flipnoteToUpdate } };
+
+    if (!isNewFlipnote) increment += prevUsers[userId][flipnoteId];
+
+    const updatedUserAnalytics = {
+      ...prevUsers,
+      ...userToUpdate,
+    };
+
+    return updatedUserAnalytics;
+  });
+}
+
+function getUserName(users: User[], userId: string) {
+  const user = users?.find((user) => user.id === userId);
+
+  return user?.name;
 }
