691ab5965e7c9098ec00414005ea6e12a80cc19a
Dakota Schramm <william.schramm11@gmail.com>
2023-09-04 14:55:52 -0500

    Fix log message spacing

--- src/app/_utils/log.ts
+++ src/app/_utils/log.ts
@@ -15,32 +15,32 @@ const log = {
   currentLevel: currentLogLevel,
   debug: (message?: any, ...optionalParams: any[]) => {
     if (currentLogLevel <= LogLevel.DEBUG) {
-      console.debug(`[${LogLevel[currentLogLevel]}] `, message, ...optionalParams)
+      console.debug(`[${LogLevel[currentLogLevel]}]`, message, ...optionalParams)
     }
   },
   info: (message?: any, ...optionalParams: any[]) => {
     if (currentLogLevel <= LogLevel.INFO) {
-      console.info(`[${LogLevel[currentLogLevel]}] `, message, ...optionalParams)
+      console.info(`[${LogLevel[currentLogLevel]}]`, message, ...optionalParams)
     }
   },
   warn: (message?: any, ...optionalParams: any[]) => {
     if (currentLogLevel <= LogLevel.WARN) {
-      console.warn(`[${LogLevel[currentLogLevel]}] `, message, ...optionalParams)
+      console.warn(`[${LogLevel[currentLogLevel]}]`, message, ...optionalParams)
     }
   },
   error: (message?: any, ...optionalParams: any[]) => {
     if (currentLogLevel <= LogLevel.ERROR) {
-      console.error(`[${LogLevel[currentLogLevel]}] `, message, ...optionalParams)
+      console.error(`[${LogLevel[currentLogLevel]}]`, message, ...optionalParams)
     }
   },
   fatal: (message?: any, ...optionalParams: any[]) => {
     if (currentLogLevel <= LogLevel.FATAL) {
-      console.error(`[${LogLevel[currentLogLevel]}] `, message, ...optionalParams)
+      console.error(`[${LogLevel[currentLogLevel]}]`, message, ...optionalParams)
     }
   },
   unknown: (message?: any, ...optionalParams: any[]) => {
     if (currentLogLevel <= LogLevel.UNKNOWN) {
-      console.log(`[${LogLevel[currentLogLevel]}] `, message, ...optionalParams)
+      console.log(`[${LogLevel[currentLogLevel]}]`, message, ...optionalParams)
     }
   },
 }
