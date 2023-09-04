a320a33954d6883a72685b44a1ab201c16aac372
Dakota Schramm <william.schramm11@gmail.com>
2023-09-04 00:58:08 -0500

    Created levels for logging messages

--- src/app/_utils/log.ts
+++ src/app/_utils/log.ts
@@ -1,4 +1,35 @@
-export default function log(message?: any, ...optionalParams: any[]) {
-  if (process.env.NODE_ENV === 'production') return
-  console.log(message, ...optionalParams)
+enum LogLevel {
+  DEBUG, 
+  INFO,
+  WARN,
+  ERROR,
+  FATAL, 
+  UNKNOWN
 }
+
+let currentLogLevel;
+if (process.env.NODE_ENV !== 'production') currentLogLevel = LogLevel.DEBUG;
+else currentLogLevel = LogLevel.WARN;
+
+const logHandlers = {
+  debug: (message?: any, ...optionalParams: any[]) => {
+    if (currentLogLevel <= LogLevel.DEBUG) console.debug(message, ...optionalParams)
+  },
+  info: (message?: any, ...optionalParams: any[]) => {
+    if (currentLogLevel <= LogLevel.INFO) console.info(message, ...optionalParams)
+  },
+  warn: (message?: any, ...optionalParams: any[]) => {
+    if (currentLogLevel <= LogLevel.WARN) console.warn(message, ...optionalParams)
+  },
+  error: (message?: any, ...optionalParams: any[]) => {
+    if (currentLogLevel <= LogLevel.ERROR) console.error(message, ...optionalParams)
+  },
+  fatal: (message?: any, ...optionalParams: any[]) => {
+    if (currentLogLevel <= LogLevel.FATAL) console.error(message, ...optionalParams)
+  },
+  unknown: (message?: any, ...optionalParams: any[]) => {
+    if (currentLogLevel <= LogLevel.UNKNOWN) console.log(message, ...optionalParams)
+  },
+}
+
+export default logHandlers;
